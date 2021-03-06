07e7836a517f9b1901b7fde7388db3863a7a4d40
juliantreweeke <juliantreweeke@gmail.com>
2021-03-06 15:22:39 +1100

    cleaned up header component

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
 import Heading from '../Heading/Index';
-import SearchBar from '../SearchForm/Index';
 import styles from "./header.module.css";
-import iTunesIcon from "../../itunes.svg";
 import EN from "../../EN.json";
 
 const Header = ({ children }) => {
   return (
-    <header>
+    <header className={styles.header}>
       <Heading size="xl" headingLevel="h1">{EN.ITUNES_ALBUMS}</Heading>
       {children}
     </header>
